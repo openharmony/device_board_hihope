3c5f5e6576a7eb24bf711971ee336e07bfceebe4
zhouke <zhouke@kaihongdigi.com>
2022-04-10 15:00:09 +0800

    增加ap6275s WiFi6驱动HDF适配代码
    Signed-off-by: zhouke <zhouke@kaihongdigi.com>

--- rk3568/wifi/bcmdhd_wifi6/hdf/hdf_mac80211_sta_event.c
+++ rk3568/wifi/bcmdhd_wifi6/hdf/hdf_mac80211_sta_event.c
@@ -52,40 +52,6 @@ int32_t HdfScanEventCallback(struct net_device *ndev, HdfWifiScanStatus _status)
     return ret;
 }
 
-#define HDF_ETHER_ADDR_LEN (6)
-int32_t HdfConnectResultEventCallback(struct net_device *ndev, uint8_t *bssid, uint8_t *reqIe,
-    uint8_t *rspIe, uint32_t reqIeLen, uint32_t rspIeLen, uint16_t connectStatus, uint16_t freq)
-{
-    int32_t retVal = 0;
-    NetDevice *netDev = GetHdfNetDeviceByLinuxInf(ndev);
-    struct ConnetResult connResult;
-    // for check p2p0 report
-    netDev = get_hdf_netdev(g_conn_event_ifidx);
-
-    HDF_LOGE("%s: enter", __func__);
-
-    if (netDev == NULL || bssid == NULL || rspIe == NULL || reqIe == NULL) {
-        HDF_LOGE("%s: netDev / bssid / rspIe / reqIe  null!", __func__);
-        return -1;
-    }
-
-    memcpy_s(&connResult.bssid[0], HDF_ETHER_ADDR_LEN, bssid, HDF_ETHER_ADDR_LEN);
-
-    connResult.rspIe = rspIe;
-    connResult.rspIeLen = rspIeLen;
-    connResult.reqIe = reqIe;
-    connResult.reqIeLen = reqIeLen;
-    connResult.connectStatus = connectStatus;
-    connResult.freq = freq;
-    connResult.statusCode = connectStatus;
-
-    retVal = HdfWifiEventConnectResult(netDev, &connResult);
-    if (retVal < 0) {
-        HDF_LOGE("%s: hdf wifi event inform connect result failed!", __func__);
-    }
-    return retVal;
-}
-
 int32_t HdfDisconnectedEventCallback(struct net_device *ndev, uint16_t reason, uint8_t *ie, uint32_t len)
 {
     int32_t ret = 0;
@@ -98,32 +64,4 @@ int32_t HdfDisconnectedEventCallback(struct net_device *ndev, uint16_t reason, u
     return ret;
 }
 
-void HdfInformBssFrameEventCallback(struct net_device *ndev, struct ieee80211_channel *channel, int32_t signal,
-    int16_t freq, struct ieee80211_mgmt *mgmt, uint32_t mgmtLen)
-{
-    int32_t retVal = 0;
-    NetDevice *netDev = GetHdfNetDeviceByLinuxInf(ndev);
-    struct ScannedBssInfo bssInfo;
-    struct WlanChannel hdfchannel;
-
-    if (channel == NULL || netDev == NULL || mgmt == NULL) {
-        HDF_LOGE("%s: inform_bss_frame channel = null or netDev = null!", __func__);
-        return;
-    }
-    netDev = get_hdf_netdev(g_scan_event_ifidx);
-    bssInfo.signal = signal;
-    bssInfo.freq = freq;
-    bssInfo.mgmtLen = mgmtLen;
-    bssInfo.mgmt = (struct Ieee80211Mgmt *)mgmt;
-
-    hdfchannel.flags = channel->flags;
-    hdfchannel.channelId = channel->hw_value;
-    hdfchannel.centerFreq = channel->center_freq;
-    retVal = HdfWifiEventInformBssFrame(netDev, &hdfchannel, &bssInfo);
-    if (retVal < 0) {
-        HDF_LOGE("%s: hdf wifi event inform bss frame failed!", __func__);
-    } else {
-        HDF_LOGE("%s: hdf wifi event inform bss frame ret = %d", __func__, retVal);
-    }
-}
 
