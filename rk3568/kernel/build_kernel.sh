2d2d6683509f5785c96e2b040c58d1f7b1dbad80
kerninfo <lijungang@kaihongdigi.com>
2022-05-09 16:11:54 +0800

    增加PRODUCT_NAME环境变量
    Signed-off-by: kerninfo <lijungang@kaihongdigi.com>

--- rk3568/kernel/build_kernel.sh
+++ rk3568/kernel/build_kernel.sh
@@ -22,6 +22,7 @@ ROOT_DIR=${5}
 export PRODUCT_PATH=${4}
 export DEVICE_COMPANY=${6}
 export DEVICE_NAME=${7}
+export PRODUCT_NAME=${8}
 
 KERNEL_SRC_TMP_PATH=${ROOT_DIR}/out/kernel/src_tmp/linux-5.10
 KERNEL_OBJ_TMP_PATH=${ROOT_DIR}/out/kernel/OBJ/linux-5.10
@@ -52,7 +53,7 @@ cp -rf ${3}/kernel/logo* ${KERNEL_SRC_TMP_PATH}/
 #config
 cp -rf ${KERNEL_CONFIG_FILE} ${KERNEL_SRC_TMP_PATH}/arch/arm64/configs/rockchip_linux_defconfig
 
-if [ "enable_ramdisk" == "${8}" ]; then
+if [ "enable_ramdisk" == "${9}" ]; then
 	./make-ohos.sh TB-RK3568X0 enable_ramdisk
 else
     ./make-ohos.sh TB-RK3568X0 disable_ramdisk
@@ -60,7 +61,7 @@ fi
 
 mkdir -p ${2}
 
-if [ "enable_ramdisk" != "${8}" ]; then
+if [ "enable_ramdisk" != "${9}" ]; then
 	cp ${KERNEL_OBJ_TMP_PATH}/boot_linux.img ${2}/boot_linux.img
 fi
 cp ${KERNEL_OBJ_TMP_PATH}/resource.img ${2}/resource.img
