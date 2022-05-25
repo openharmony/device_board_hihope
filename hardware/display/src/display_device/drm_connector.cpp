bd6f473a7375baf6633fd97115e5079cd56e9349
muxiaoximuxiaoxi <sunhaohao5@huawei.com>
2022-05-25 07:11:37 -0700

    optimize display log
    Signed-off-by: muxiaoximuxiaoxi <sunhaohao5@huawei.com>

--- hardware/display/src/display_device/drm_connector.cpp
+++ hardware/display/src/display_device/drm_connector.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -125,7 +125,7 @@ int32_t DrmConnector::SetBrightness(uint32_t level)
     if (brFd <= 0) {
         brFd = open("/sys/class/backlight/backlight/brightness", O_RDWR);
         if (brFd < 0) {
-            DISPLAY_LOGE("oepn Brightness file failed\n");
+            DISPLAY_LOGE("open brightness file failed\n");
             return DISPLAY_NOT_SUPPORT;
         }
     }
