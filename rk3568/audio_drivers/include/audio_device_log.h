bfe49d7c3d66f94602d0a96703e3619de31d45b0
wangyeyu <wangyeyu1@huawei.com>
2022-07-14 14:43:47 +0800

    fix:Unified audio log style
    Signed-off-by: wangyeyu <wangyeyu1@huawei.com>

--- rk3568/audio_drivers/include/audio_device_log.h
+++ rk3568/audio_drivers/include/audio_device_log.h
@@ -15,22 +15,32 @@
 extern "C" {
 #endif
 #endif /* __cplusplus */
+// 1 is on; 0 is off
+#define AUDIO_DEVICE_DEBUG_ON 0
 
 #define AUDIO_DEVICE_LOG_ERR(fmt, arg...) do { \
     HDF_LOGE("[%s][line:%d]: " fmt, __func__, __LINE__, ##arg); \
     } while (0)
 
-#define AUDIO_DEVICE_LOG_INFO(fmt, arg...) do { \
-    HDF_LOGE("[%s][line:%d]: " fmt, __func__, __LINE__, ##arg); \
+#define AUDIO_DEVICE_LOG_WARNING(fmt, arg...) do { \
+    HDF_LOGW("[%s][line:%d]: " fmt, __func__, __LINE__, ##arg); \
     } while (0)
 
-#define AUDIO_DEVICE_LOG_WARNING(fmt, arg...) do { \
-    HDF_LOGE("[%s][line:%d]: " fmt, __func__, __LINE__, ##arg); \
+#if (AUDIO_DEVICE_DEBUG_ON)
+#define AUDIO_DEVICE_LOG_INFO(fmt, arg...) do { \
+    HDF_LOGI("[%s][line:%d]: " fmt, __func__, __LINE__, ##arg); \
     } while (0)
 
 #define AUDIO_DEVICE_LOG_DEBUG(fmt, arg...) do { \
-    HDF_LOGE("[%s][line:%d]: " fmt, __func__, __LINE__, ##arg); \
+    HDF_LOGD("[%s][line:%d]: " fmt, __func__, __LINE__, ##arg); \
     } while (0)
+#else
+#define AUDIO_DEVICE_LOG_INFO(fmt, arg...) do { \
+    } while (0)
+
+#define AUDIO_DEVICE_LOG_DEBUG(fmt, arg...) do { \
+    } while (0)
+#endif
 
 #ifdef __cplusplus
 #if __cplusplus
