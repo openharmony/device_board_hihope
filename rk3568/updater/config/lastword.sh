167e99c920f4bd8e5e2e163e62bcb37f0b789d40
qy136 <18772452388@163.com>
2022-09-05 19:30:40 +0800

    add hilog adn faultlogger
    Signed-off-by: qy136 <18772452388@163.com>

--- rk3568/updater/config/lastword.sh
+++ rk3568/updater/config/lastword.sh
@@ -1,5 +1,20 @@
 mkdir /backdata
 mount /dev/block/by-name/userdata /backdata
-rm -rf /backdata/updater/log/*.gz
-cp /data/log/hilog/updater* /backdata/updater/log
-cp /data/log/faultlog/temp/cpp* /backdata/updater/log
+
+if [ -d "/backdata/updater/log/faultlog" ];
+then
+    rm -rf /backdata/updater/log/faultlog/*
+    cp /data/log/faultlog/temp/cpp* /backdata/updater/log/faultlog
+else
+    mkdir /backdata/updater/log/faultlog
+    cp /data/log/faultlog/temp/cpp* /backdata/updater/log/faultlog
+fi
+
+if [ -d "/backdata/updater/log/hilog" ];
+then
+    rm -rf /backdata/updater/log/hilog/*
+    cp /data/log/hilog/updater* /backdata/updater/log/hilog
+else
+    mkdir /backdata/updater/log/hilog
+    cp /data/log/hilog/updater* /backdata/updater/log/hilog
+fi
