9b80935e06b69c11f90f1aa5d5af4f45fe6bc1cd
zhouge94 <zhouge7@huawei.com>
2024-03-25 20:49:21 +0800

    modify pipeline to fit single video stream in device hihope
    Signed-off-by: zhouge94 <zhouge7@huawei.com>

--- rk3568/camera/vdi_impl/v4l2/pipeline_core/src/node/rk_scale_node.cpp
+++ rk3568/camera/vdi_impl/v4l2/pipeline_core/src/node/rk_scale_node.cpp
@@ -12,6 +12,7 @@
  */
 
 #include "rk_scale_node.h"
+#include "rk_node_utils.h"
 #include <securec.h>
 
 namespace OHOS::Camera {
@@ -29,24 +30,6 @@ RKScaleNode::~RKScaleNode()
 RetCode RKScaleNode::Start(const int32_t streamId)
 {
     CAMERA_LOGI("RKScaleNode::Start streamId = %{public}d\n", streamId);
-    uint64_t bufferPoolId = 0;
-
-    outPutPorts_ = GetOutPorts();
-    for (auto& out : outPutPorts_) {
-        bufferPoolId = out->format_.bufferPoolId_;
-    }
-
-    BufferManager* bufferManager = Camera::BufferManager::GetInstance();
-    if (bufferManager == nullptr) {
-        CAMERA_LOGE("scale buffer get instance failed");
-        return RC_ERROR;
-    }
-
-    bufferPool_ = bufferManager->GetBufferPool(bufferPoolId);
-    if (bufferPool_ == nullptr) {
-        CAMERA_LOGE("get bufferpool failed: %{public}zu", bufferPoolId);
-        return RC_ERROR;
-    }
     return RC_OK;
 }
 
@@ -62,85 +45,6 @@ RetCode RKScaleNode::Flush(const int32_t streamId)
     return RC_OK;
 }
 
-void RKScaleNode::PreviewScaleConver(std::shared_ptr<IBuffer>& buffer)
-{
-    if (buffer == nullptr) {
-        CAMERA_LOGE("RKScaleNode::PreviewScaleConver buffer == nullptr");
-        return;
-    }
-
-    int dma_fd = buffer->GetFileDescriptor();
-    uint8_t* temp = (uint8_t *)buffer->GetVirAddress();
-
-    std::map<int32_t, uint8_t*> sizeVirMap = bufferPool_->getSFBuffer(buffer->GetIndex());
-    if (sizeVirMap.empty()) {
-        return;
-    }
-    uint8_t* virBUffer = sizeVirMap.begin()->second;
-    int32_t virSize = sizeVirMap.begin()->first;
-    buffer->SetVirAddress(virBUffer);
-    buffer->SetSize(virSize);
-
-    RockchipRga rkRga;
-
-    rga_info_t src = {};
-    rga_info_t dst = {};
-
-    src.fd = -1;
-    src.mmuFlag = 1;
-    src.rotation = 0;
-    src.virAddr = (void *)temp;
-
-    dst.fd = dma_fd;
-    dst.mmuFlag = 1;
-    dst.virAddr = 0;
-
-    rga_set_rect(&src.rect, 0, 0, wide_, high_, wide_, high_, RK_FORMAT_YCbCr_420_P);
-    rga_set_rect(&dst.rect, 0, 0, buffer->GetWidth(), buffer->GetHeight(),
-        buffer->GetWidth(), buffer->GetHeight(), RK_FORMAT_YCbCr_420_P);
-
-    rkRga.RkRgaBlit(&src, &dst, NULL);
-    rkRga.RkRgaFlush();
-}
-
-void RKScaleNode::ScaleConver(std::shared_ptr<IBuffer>& buffer)
-{
-    if (buffer == nullptr) {
-        CAMERA_LOGE("RKScaleNode::ScaleConver buffer == nullptr");
-        return;
-    }
-
-    int dma_fd = buffer->GetFileDescriptor();
-
-    std::map<int32_t, uint8_t*> sizeVirMap = bufferPool_->getSFBuffer(bufferPool_->GetForkBufferId());
-    if (sizeVirMap.empty()) {
-        CAMERA_LOGE("RKScaleNode::ScaleConver sizeVirMap buffer == nullptr");
-        return;
-    }
-    uint8_t* temp = sizeVirMap.begin()->second;
-
-    RockchipRga rkRga;
-
-    rga_info_t src = {};
-    rga_info_t dst = {};
-
-    src.fd = -1;
-    src.mmuFlag = 1;
-    src.rotation = 0;
-    src.virAddr = (void *)temp;
-
-    dst.fd = dma_fd;
-    dst.mmuFlag = 1;
-    dst.virAddr = 0;
-
-    rga_set_rect(&src.rect, 0, 0, wide_, high_, wide_, high_, RK_FORMAT_YCbCr_420_P);
-    rga_set_rect(&dst.rect, 0, 0, buffer->GetWidth(), buffer->GetHeight(),
-        buffer->GetWidth(), buffer->GetHeight(), RK_FORMAT_YCbCr_420_P);
-
-    rkRga.RkRgaBlit(&src, &dst, NULL);
-    rkRga.RkRgaFlush();
-}
-
 void RKScaleNode::DeliverBuffer(std::shared_ptr<IBuffer>& buffer)
 {
     if (buffer == nullptr) {
@@ -148,26 +52,22 @@ void RKScaleNode::DeliverBuffer(std::shared_ptr<IBuffer>& buffer)
         return;
     }
 
+    if (buffer->GetBufferStatus() != CAMERA_BUFFER_STATUS_OK) {
+        CAMERA_LOGE("BufferStatus() != CAMERA_BUFFER_STATUS_OK");
+        return NodeBase::DeliverBuffer(buffer);
+    }
+
     int32_t id = buffer->GetStreamId();
-    CAMERA_LOGI("RKScaleNode::DeliverBuffer StreamId %{public}d", id);
+    CAMERA_LOGI("RKScaleNode::DeliverBuffer, streamId[%{public}d],\
+index[%{public}d], %{public}d * %{public}d ==> %{public}d * %{public}d, encodeType = %{public}d", 
+        buffer->GetStreamId(), buffer->GetIndex(),
+        buffer->GetCurWidth(), buffer->GetCurHeight(), buffer->GetWidth(), buffer->GetHeight(),
+        buffer->GetEncodeType());
 
-    if (bufferPool_->GetForkBufferId() != -1) {
-        if (bufferPool_->GetIsFork() == true) {
-            ScaleConver(buffer);
-        } else {
-            PreviewScaleConver(buffer);
-        }
+    if (buffer->GetEncodeType() == ENCODE_TYPE_NULL) {
+        RkNodeUtils::BufferScaleFormatTransform(buffer);
     }
-
-    std::vector<std::shared_ptr<IPort>> outPutPorts_;
-    outPutPorts_ = GetOutPorts();
-    for (auto& it : outPutPorts_) {
-        if (it->format_.streamId_ == id) {
-            it->DeliverBuffer(buffer);
-            CAMERA_LOGI("RKScaleNode deliver buffer streamid = %{public}d", it->format_.streamId_);
-            return;
-        }
-    }
+    NodeBase::DeliverBuffer(buffer);
 }
 
 RetCode RKScaleNode::Capture(const int32_t streamId, const int32_t captureId)
